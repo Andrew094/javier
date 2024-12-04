85dce9d18dacfb7f88a28aef39097e83d8b96995
Andreas Salvermoser <andreas@developer.com>
2024-12-04 16:04:59 -0500

    init wordpress

--- index.php
+++ index.php
@@ -1,44 +1,17 @@
 <?php
 /**
- * The main template file.
- * @package Viral News
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
  */
-get_header();
-?>
 
-<div class="vn-container vn-clearfix">
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-    <div id="primary" class="content-area">
-
-        <?php if (have_posts()): ?>
-
-            <?php if (is_home() && !is_front_page()): ?>
-                <header class="vn-main-header">
-                    <h1><?php single_post_title(); ?></h1>
-                </header>
-            <?php endif; ?>
-
-            <?php while (have_posts()):
-                the_post(); ?>
-
-                <?php
-                get_template_part('template-parts/content');
-                ?>
-
-            <?php endwhile; ?>
-
-            <?php the_posts_pagination(); ?>
-
-        <?php else: ?>
-
-            <?php get_template_part('template-parts/content', 'none'); ?>
-
-        <?php endif; ?>
-
-    </div><!-- #primary -->
-
-    <?php get_sidebar(); ?>
-</div>
-
-<?php
-get_footer();
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
